7d4e298a25fdcf0ae40e8b3da9f1b76212494a32
Chase Moskal <chasemoskal@gmail.com>
2026-03-29 15:23:21 -0700

    sketch: new generate fn

--- s/lib/gridworld/generate.ts
+++ s/lib/gridworld/generate.ts
@@ -1,14 +1,33 @@
 
 import {Randy, Vec2} from "@benev/math"
-import {Gridworld, TileKind} from "./types.js"
-import {connectWaypointsWithFlooring, drawBorder, drawSplotchyTileSubstrate, initGridworld} from "./utils/utils.js"
+import {Gridworld} from "./types.js"
+import {initGridworld} from "./utils/utils.js"
 
 export const generateGridworld = (seed: number, extent: Vec2): Gridworld => {
 	const grid = initGridworld(extent)
 	const randy = new Randy(seed)
-	drawSplotchyTileSubstrate(grid, randy)
-	connectWaypointsWithFlooring(grid, randy)
-	drawBorder(grid, 1, TileKind.Floor)
+	const navel = getRandomPointNearMiddle(grid, randy, 0.25)
+
+	splotchySubstrate({
+		grid,
+		randy,
+		walls: {
+			probability: 0.5,
+			scales: [0.01, 0.2],
+		},
+		pits: {
+			probability: 0.25,
+			scales: [0.01, 0.2],
+		},
+	})
+
+	carveDrunkenPathsBetweenWaypoints(
+		getPointsAroundBorder()
+			.map(point => ({from: point, to: navel}))
+	)
+
+	southernFlooringGradient(grid, randy, 0.25)
+
 	return grid
 }
 
