f01dab167183bf7d111f7bc7f8011f3b76ff90db
Chase Moskal <chasemoskal@gmail.com>
2026-03-30 18:37:07 -0700

    tweak: html template

--- s/index.html.ts
+++ s/index.html.ts
@@ -5,26 +5,24 @@ export default template(import.meta.url, async orb => html`
 	<!doctype html>
 	<html>
 		<head>
+			<title>nano</title>
+			<link rel="icon" href="${dataSvgEmoji("🧬")}"/>
+
 			<meta charset="utf-8"/>
 			<meta name="viewport" content="width=device-width,initial-scale=1"/>
 			<meta name="darkreader-lock"/>
-			<style>@layer basic{html{background:#000}}</style>
-
-			<title>nano</title>
-			<link rel="icon" href="${dataSvgEmoji("🧬")}"/>
-			<script type="module" src="${orb.hashurl("client/main.bundle.min.js")}"></script>
 
 			<style data-theme>${orb.inject("client/styles/layers.css")}</style>
 			<style data-theme>${orb.inject("client/styles/vars.css")}</style>
 			<style data-theme>${orb.inject("client/styles/basics.css")}</style>
 			<style>${orb.inject("client/styles/page.css")}</style>
 
+			<script type="module" src="${orb.hashurl("client/main.bundle.min.js")}"></script>
+
 			${socialCard({
-				themeColor: "#8FCC8F",
 				title: "nano",
 				description: "a little game",
-				siteName: "https://nano.benev.gg/",
-				image: `https://nano.benev.gg/favicon.png`,
+				themeColor: "#307ba0",
 			})}
 		</head>
 		<body>
