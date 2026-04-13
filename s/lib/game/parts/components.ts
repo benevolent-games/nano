d7d5daa3867b991750d41e5dad8cea079ec1ee66
Chase Moskal <chasemoskal@gmail.com>
2026-04-13 11:44:54 -0700

    add: components doc comments

--- s/lib/game/parts/components.ts
+++ s/lib/game/parts/components.ts
@@ -3,17 +3,44 @@ import {XyArray} from "@benev/math"
 import {AsComponents} from "@benev/archimedes"
 
 export type GameComponents = AsComponents<{
+
+	/** section of grid tiles, compactly encoded as a hex string */
 	gridchunk: string
+
+	/** centerpoint position in gridspace coordinates */
 	position: XyArray
+
+	/** angular rotation in radians, counter clockwise */
 	rotation: number
+
+	/** enabled if user inputs should exert influence */
 	controllable: boolean
+
+	/** how much oomph this entity has when it tries to move */
 	speed: number
+
+	/** direction where the user wants to move */
+	intent: XyArray
+
+	/** direction this entity is being pushed */
 	force: XyArray
+
+	/** which 3d model to display */
 	graphic: "robot"
+
+	/** enabled if this entity can bump into things */
 	physical: boolean
+
+	/** rectangular extent */
 	size: XyArray
+
+	/** how heavy is this entity */
 	mass: number
+
+	/** circular size */
 	radius: number
+
+	/** lerp factor for smoothing movements */
 	lerp: number
 }>
 
