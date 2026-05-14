24fa8db1f473d53f26331e1448e04c9e7c2b9367
Chase Moskal <chasemoskal@gmail.com>
2026-05-13 19:00:32 -0700

    tweak: reseed rand every tick

--- s/lib/game/systems/timing_update.ts
+++ s/lib/game/systems/timing_update.ts
@@ -1,7 +1,9 @@
 
+import {seed} from "@benev/math"
 import {Pod} from "../parts/pod.js"
 
 export const timing_update = (pod: Pod) => () => {
 	pod.timing.update()
+	pod.rand.random = seed(Math.floor(pod.timing.tick / 60))
 }
 
