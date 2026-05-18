c07647fdb1113b81732db175d4b187024b3b210a
Chase Moskal <chasemoskal@gmail.com>
2026-05-18 02:11:09 -0700

    fix: gridgen to use consts map params

--- s/client/views/gridgen/parts/use-gridworld.ts
+++ s/client/views/gridgen/parts/use-gridworld.ts
@@ -3,12 +3,13 @@ import {Vec2} from "@benev/math"
 import {debounce} from "@e280/stz"
 import {Signal} from "@e280/strata"
 import {useEffect, useSignal} from "@e280/sly"
+import {consts} from "../../../../consts.js"
 import {generateGridworld} from "../../../../lib/gridworld/generate.js"
 
 export function useGridworld() {
-	const $seed: Signal<number> = useSignal(1)
-	const $x = useSignal(256)
-	const $y = useSignal(256)
+	const $seed: Signal<number> = useSignal(consts.map.seed)
+	const $x = useSignal(consts.map.extent.x)
+	const $y = useSignal(consts.map.extent.y)
 
 	const $gridworld = useSignal(generateGridworld($seed(), new Vec2($x(), $y())))
 	const $generationMs = useSignal(0)
